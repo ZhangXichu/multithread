bb040ebfc54e509f09486dc3427acf0f29a67ba9
ZhangXichu <xichu.zhang@mensa.cz>
2023-10-15 15:29:03 +0200

    data parallelism

---
+++ include/data_parallelism.h
@@ -0,0 +1,11 @@
+# ifndef DATA_PARALLELISM_H
+# define DATA_PARALLELISM_H
+
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <future>
+#include <random>
+
+# endif
---
+++ std_algorithm.cpp
@@ -0,0 +1,10 @@
+#include "include/std_algorithm.h"
+
+int main() {
+
+    std::string str = "This is a string";
+
+    traverse(str.begin(), str.end());
+
+    return 0;
+}
