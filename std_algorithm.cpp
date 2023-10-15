6223817a8fce28a37966dbab519e9c9daab05f0f
ZhangXichu <xichu.zhang@mensa.cz>
2023-10-15 16:16:57 +0200

    use cbegin and cend instead

--- std_algorithm.cpp
+++ std_algorithm.cpp
@@ -4,7 +4,7 @@ int main() {
 
     std::string str = "This is a string";
 
-    int res_i = find_first_occur(str.begin(), str.end(), 'i');
+    int res_i = find_first_occur(str.cbegin(), str.cend(), 'i');
 
     std::cout << "The 1st occurence of the given char is found at index: " << res_i << std::endl;
 
