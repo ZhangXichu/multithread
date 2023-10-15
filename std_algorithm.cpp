bcfd9b7fd2eefb5301bae11d1229298021cd7259
ZhangXichu <xichu.zhang@mensa.cz>
2023-10-15 15:46:34 +0200

    minor change

--- std_algorithm.cpp
+++ std_algorithm.cpp
@@ -4,7 +4,9 @@ int main() {
 
     std::string str = "This is a string";
 
-    traverse(str.begin(), str.end());
+    int res_i = find_first_occur(str.begin(), str.end(), 'i');
+
+    std::cout << "The 1st occurence of the given char is found at index: " << res_i << std::endl;
 
     return 0;
 }
